a48b830df49abc62f6c458ae2288f40563219171
Hugo <will.have.revange@gmail.com>
2021-06-01 11:35:20 -0300

    quarto commit de teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FB from 'fb';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount()  {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '223113992581235',
+        cookie     : true,  
+        xfbml      : true,  
+        version    : 'v2.1' 
+      });
+    }
+  }
+
+  render() {
+    console.log('teste')
+    FB.api('/me', function(response) {
+      console.log(JSON.stringify(response));
+    });
+
+    return (
+      <div className="App">
+        <h1>teste</h1>
+      </div>
+    );
+
+  }
 }
 
 export default App;
