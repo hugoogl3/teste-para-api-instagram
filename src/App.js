0e0d093e6251d88d755e4c0398f048b319beaf10
Hugo <will.have.revange@gmail.com>
2021-06-15 17:24:55 -0300

    novo commit 15 do 06

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FB from 'fb';
 import { render } from 'react-dom';
 
+let Teste = "teste"
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -18,12 +19,26 @@ class App extends React.Component {
       });
     }
   }
-
+  
   render() {
-    console.log(FB.getLoginStatus)
+    FB.api(
+      '/oauth/authorize',
+      'GET',
+      {
+        "client_id": "223113992581235",
+        "redirect_uri": "https://www.hugodk33.github.io/cv",
+        "scope": 'code',
+        "access_token": "EAADK68ZAFsHMBAHvL9e2SJPVRisNyCZB8ZAIABrPBa6QRdoyujgh88zDF5QptwbTFJJllDaYu7wQT8frN3MMRTCbGhB0JhaX22qwp4rXWUwEKVA4eo68ZB9yhgYDPfZCnawzdAd9vPPiDekn5CoARtZAMDT5ehgVzoPqjqlLdCZB10sGjmItI8ytoUwm4O8UrieZBE0DAGjdOQZDZD" 
+      },
+      function(response) {
+        console.log("resposta")
+        console.log(response)
+      }
+    );
+
     return (
       <div className="App">
-        <h1>teste</h1>
+        <h1>testado API do instagram</h1>
       </div>
     );
 
