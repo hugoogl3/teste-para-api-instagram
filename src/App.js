55b6b74242f60c7b15c891a6cf14df6fb644bad9
Hugo <will.have.revange@gmail.com>
2021-06-17 10:22:02 -0300

    teste do dia 17 do 6, testando o app de exibição básica do instagram

--- src/App.js
+++ src/App.js
@@ -19,17 +19,19 @@ class App extends React.Component {
       });
     }
   }
-  
+  ///oauth/authorize
   render() {
     FB.api(
       '/oauth/authorize',
       'GET',
+
       {
         "client_id": "223113992581235",
-        "redirect_uri": "https://www.hugodk33.github.io/cv",
+        "redirect_uri": "https://hugoogl3.github.io/teste-para-api-instagram/",
         "scope": 'code',
-        "access_token": "EAADK68ZAFsHMBAHvL9e2SJPVRisNyCZB8ZAIABrPBa6QRdoyujgh88zDF5QptwbTFJJllDaYu7wQT8frN3MMRTCbGhB0JhaX22qwp4rXWUwEKVA4eo68ZB9yhgYDPfZCnawzdAd9vPPiDekn5CoARtZAMDT5ehgVzoPqjqlLdCZB10sGjmItI8ytoUwm4O8UrieZBE0DAGjdOQZDZD" 
+        "access_token": "EAADK68ZAFsHMBABSwarwq0wNahOf5OUeXTIZAQpumJsHkgomcxksIxso2YH1RYGEAHe85lkhdfZCgb2VC6kLiwUR5ZBzZC3HOZChvim8EJwF4lEhgfbYhLkXt5rZCF1RewXoDoDufQNbwzDyjwXzvxvLX0VcDVrD03EVNZBriK6olIFg2I9bBftMkiGZCwIzCLmhIM10aRFTC7gZDZD" 
       },
+
       function(response) {
         console.log("resposta")
         console.log(response)
